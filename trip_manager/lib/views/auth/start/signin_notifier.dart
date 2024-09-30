b553c2a949098c9f4362b8f44cd70e4141d8985a
chaenchaen99 <chaenjung.99@gmail.com>
2024-09-30 22:28:16 +0900

    구글 SSO 플랫폼 연동

---
+++ trip_manager/lib/views/auth/start/signin_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:trip_manager/models/auth/user_profile.dart';
+
+part 'signin_notifier.g.dart';
+
+@riverpod
+class SigninNotifier extends _$SigninNotifier {
+  @override
+  UserProfile? build() {
+    return null;
+  }
+
+  void setUserProfile(UserProfile? profile) {
+    state = profile;
+  }
+
+  void clearUserProfile() {
+    state = null;
+  }
+}
---
+++ trip_manager/lib/views/auth/start/signin_notifier.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'signin_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$signinNotifierHash() => r'0bce003b805c973bb04ff7a7a701dc9e1fcb1130';
+
+/// See also [SigninNotifier].
+@ProviderFor(SigninNotifier)
+final signinNotifierProvider =
+    AutoDisposeNotifierProvider<SigninNotifier, UserProfile?>.internal(
+  SigninNotifier.new,
+  name: r'signinNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$signinNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$SigninNotifier = AutoDisposeNotifier<UserProfile?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
