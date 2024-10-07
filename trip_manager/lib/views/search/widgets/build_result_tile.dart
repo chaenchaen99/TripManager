0e02fb55e8442e5e83dc87490f867fa11e7ba1eb
chaeyeon <chaenjung.99@gmail.com>
2024-10-07 12:40:34 +0900

    fix) search 뷰 ListTile UI 수정

--- trip_manager/lib/views/search/widgets/build_result_tile.dart
+++ trip_manager/lib/views/search/widgets/build_result_tile.dart
@@ -16,77 +16,114 @@ class BuildResultTile extends ConsumerWidget {
     return ListTile(
       contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 18.0),
       dense: true,
-      title: Row(
-        children: [
-          item.spaceType == SpaceType.region
-              ? CircularIconWidget()
-              : Image.asset(
-                  'assets/icons/schedule.png',
-                  width: 20,
-                  height: 20,
+      title: item.spaceType == SpaceType.region
+          ? RegionItem(item: item)
+          : Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8), // roundRadius 적용
+                  child: Container(
+                    width: 56.w,
+                    height: 56.h,
+                    color: AppColors.lightColor_2, // URL이 빈 문자열일 때 회색 배경 적용
+                  ),
                 ),
-          SizedBox(width: 12),
-          Column(children: [
-            Row(children: [
-              Text(
-                item.name,
-                style: TextStyle(fontSize: 16.sp),
-              ),
-              Text(
-                //TODO: 실데이터로 변경
-                "1255",
-                style: TextStyle(fontSize: 12.sp),
-              ),
-            ]),
-            Text(
-              //TODO: 실데이터로 변경
-              "서울",
-              style: TextStyle(fontSize: 12.sp),
+                SizedBox(width: 12),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      item.name,
+                      style: TextStyle(fontSize: 16.sp),
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      "서울",
+                      style: TextStyle(
+                        fontSize: 12.sp,
+                        color: AppColors.darkColor_3,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ]),
-          Spacer(),
-          GestureDetector(
-            onTap: () {
-              ref
-                  .read(searchHistoryProvider.notifier)
-                  .clearSearchHistory(item.name);
-            },
-            child: Container(
-              padding: EdgeInsets.all(9),
-              child: Image.asset(
-                'assets/icons/close.png',
-                width: 11,
-                height: 11,
-              ),
-            ),
-          ),
-        ],
-      ),
       onTap: () {},
     );
   }
 }
 
-class CircularIconWidget extends StatelessWidget {
+class RegionItem extends ConsumerWidget {
+  const RegionItem({
+    super.key,
+    required this.item,
+  });
+
+  final FilterResult item;
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 36.0, // 원형 위젯의 너비
-      height: 36.0, // 원형 위젯의 높이
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(color: AppColors.lightColor_2), // 테두리 색상
-      ),
-      child: ClipOval(
-        child: Center(
-          child: Image.asset(
-            'assets/icons/location_empty.png', // 위치 아이콘 경로
-            width: 20.w, // 아이콘의 너비
-            height: 20.h, // 아이콘의 높이
-            fit: BoxFit.contain, // 아이콘 크기 조정 방식
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Row(
+      children: [
+        Container(
+          width: 36.0, // 원형 위젯의 너비
+          height: 36.0, // 원형 위젯의 높이
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: AppColors.lightColor_2), // 테두리 색상
+          ),
+          child: ClipOval(
+            child: Center(
+              child: Image.asset(
+                'assets/icons/location_empty.png', // 위치 아이콘 경로
+                width: 20.w, // 아이콘의 너비
+                height: 20.h, // 아이콘의 높이
+                fit: BoxFit.contain, // 아이콘 크기 조정 방식
+              ),
+            ),
           ),
         ),
-      ),
+        SizedBox(width: 12),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(children: [
+            Text(
+              item.name,
+              style: TextStyle(fontSize: 16.sp),
+            ),
+            SizedBox(width: 4),
+            Text(
+              "1255",
+              style: TextStyle(
+                fontSize: 12.sp,
+                color: AppColors.darkColor_3,
+              ),
+            ),
+          ]),
+          Text(
+            "서울",
+            style: TextStyle(
+              fontSize: 12.sp,
+              color: AppColors.darkColor_3,
+            ),
+          ),
+        ]),
+        Spacer(),
+        GestureDetector(
+          onTap: () {
+            ref
+                .read(searchHistoryProvider.notifier)
+                .clearSearchHistory(item.name);
+          },
+          child: Container(
+            padding: EdgeInsets.all(9),
+            child: Image.asset(
+              'assets/icons/close.png',
+              width: 11,
+              height: 11,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
